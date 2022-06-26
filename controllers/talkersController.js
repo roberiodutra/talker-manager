7bef56729bdc0537735627d07f1a05108bfa2809
roberio <roberio-almeida@hotmail.com>
2022-06-26 17:15:48 -0300

    feat: create controller to search talker by ID

--- controllers/talkersController.js
+++ controllers/talkersController.js
@@ -2,6 +2,7 @@ const talkersService = require('../services/talkersService');
 const httpStatus = require('../helpers/httpStatusCodes');
 
 const TALKERS_FILE = './talker.json';
+const INTERNAL_ERROR_MESSAGE = { message: 'Error when trying to perform operation' };
 
 const getAllTalkers = async (_req, res) => {
   try {
@@ -11,7 +12,21 @@ const getAllTalkers = async (_req, res) => {
     : res.status(httpStatus.OK).json(talkersList);
   } catch (err) {
     console.error(err);
-    res.status(httpStatus.INTERNAL_SERVER).json({ message: 'Error when trying to perform operation' });
+    res.status(httpStatus.INTERNAL_SERVER).json(INTERNAL_ERROR_MESSAGE);
+  }
+};
+
+const getTalkerById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talkersList = await talkersService.getAllTalkers(TALKERS_FILE);
+    const talkerFound = talkersList.find((tk) => tk.id === +id);
+    !talkerFound
+    ? res.status(httpStatus.NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' })
+    : res.status(httpStatus.OK).json(talkerFound);
+  } catch (err) {
+    console.error(err);
+    res.status(httpStatus.INTERNAL_SERVER).json(INTERNAL_ERROR_MESSAGE);
   }
 };
 

29b3f093891c990f3d03eb344d6d789dadcb654a
roberio <roberio-almeida@hotmail.com>
2022-06-26 15:50:11 -0300

    feat: configuring talkers controller
    configuring controller and returning the requested data or errors

---
+++ controllers/talkersController.js
@@ -0,0 +1,18 @@
+const talkersService = require('../services/talkersService');
+const httpStatus = require('../helpers/httpStatusCodes');
+
+const TALKERS_FILE = './talker.json';
+
+const getAllTalkers = async (_req, res) => {
+  try {
+    const talkersList = await talkersService.getAllTalkers(TALKERS_FILE);
+    !talkersList
+    ? res.status(httpStatus.NOT_FOUND).json({ message: 'No talkers found' })
+    : res.status(httpStatus.OK).json(talkersList);
+  } catch (err) {
+    console.error(err);
+    res.status(httpStatus.INTERNAL_SERVER).json({ message: 'Error when trying to perform operation' });
+  }
+};
+
+module.exports = { getAllTalkers };

85c9e0b0e1572119f5b1c780a563ce41e5b38cb6
roberio <roberio-almeida@hotmail.com>
2022-06-26 15:33:46 -0300

    feat: add helmet, rescue, and morgan
    create routers and config index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('./routers');
+const rescue = require('express-rescue');
+const helmet = require("helmet");
+const morgan = require("morgan");
+const httpStatus = require('./helpers/httpStatusCodes');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan("common"));
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(httpStatus.OK).send();
 });
 
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.use(rescue(router.talkersRouter));

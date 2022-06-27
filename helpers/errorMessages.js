fc238aeed2a2e58e66d9310f632ae89d2be35588
roberio <roberio-almeida@hotmail.com>
2022-06-26 22:56:37 -0300

    feat: adding more messages for next requeriments errors

--- helpers/errorMessages.js
+++ helpers/errorMessages.js
@@ -5,7 +5,18 @@ const errorMessages = {
   EMAIL_REQUIRED: 'O campo "email" é obrigatório',
   INVALID_EMAIL: 'O "email" deve ter o formato "email@email.com"',
   PASSWORD_REQUIRED: 'O campo "password" é obrigatório',
-  INVALID_PASSWORD: 'O "password" deve ter pelo menos 6 caracteres'
+  INVALID_PASSWORD: 'O "password" deve ter pelo menos 6 caracteres',
+  TOKEN_NOT_FOUND: 'Token não encontrado',
+  INVALID_TOKEN: 'Token inválido',
+  NAME_REQUIRED: 'O campo "name" é obrigatório',
+  INVALID_NAME: 'O "name" deve ter pelo menos 3 caracteres',
+  AGE_REQUIRED: 'O campo "age" é obrigatório',
+  INVALID_AGE: 'A pessoa palestrante deve ser maior de idade',
+  TALK_REQUIRED: 'O campo "talk" é obrigatório',
+  DATE_REQUIRED: 'O campo "watchedAt" é obrigatório',
+  INVALID_DATE: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+  RATE_REQUIRED: 'O campo "rate" é obrigatório',
+  INVALID_RATE: 'O campo "rate" deve ser um inteiro de 1 à 5'
 };
 
 module.exports = errorMessages;

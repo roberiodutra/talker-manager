7060832023e7736937d6629ca31b362372387691
roberio <roberio-almeida@hotmail.com>
2022-06-26 22:31:44 -0300

    feat: create file for status code message

--- controllers/talkersController.js
+++ controllers/talkersController.js
@@ -1,9 +1,8 @@
 const talkersService = require('../services/talkersService');
-const httpStatus = require('../helpers/httpStatusCodes');
+const { httpStatus, errorMessages } = require('../helpers');
 const str = require('@supercharge/strings');
 
 const TALKERS_FILE = './talker.json';
-const INTERNAL_ERROR_MESSAGE = { message: 'Error when trying to perform operation' };
 
 const randToken = (nbr) => str.random(nbr);
 
@@ -11,11 +10,11 @@ const getAllTalkers = async (_req, res) => {
   try {
     const talkersList = await talkersService.getAllTalkers(TALKERS_FILE);
     !talkersList
-    ? res.status(httpStatus.NOT_FOUND).json({ message: '404 Nothing Found' })
+    ? res.status(httpStatus.NOT_FOUND).json({ message: errorMessages.NOT_FOUND })
     : res.status(httpStatus.OK).json(talkersList);
   } catch (err) {
     console.error(err);
-    res.status(httpStatus.INTERNAL_SERVER).json(INTERNAL_ERROR_MESSAGE);
+    res.status(httpStatus.INTERNAL_SERVER).json({ message: errorMessages.INTERNAL_ERROR });
   }
 };
 
@@ -25,11 +24,11 @@ const getTalkerById = async (req, res) => {
     const talkersList = await talkersService.getAllTalkers(TALKERS_FILE);
     const talkerFound = talkersList.find((tk) => tk.id === +id);
     !talkerFound
-    ? res.status(httpStatus.NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' })
+    ? res.status(httpStatus.NOT_FOUND).json({ message: errorMessages.TALKER_NOT_FOUND })
     : res.status(httpStatus.OK).json(talkerFound);
   } catch (err) {
     console.error(err);
-    res.status(httpStatus.INTERNAL_SERVER).json(INTERNAL_ERROR_MESSAGE);
+    res.status(httpStatus.INTERNAL_SERVER).json({ message: errorMessages.INTERNAL_ERROR });
   }
 };
 
@@ -39,12 +38,12 @@ const userLogin = (req, res) => {
     const invalidLogin = talkersService.validateLogin(email, password);
 
     invalidLogin
-    ? res.status(httpStatus.BAD_REQUEST).json(invalidLogin)
+    ? res.status(httpStatus.BAD_REQUEST).json({ message: invalidLogin })
     : res.status(httpStatus.OK).json({ token: randToken(16) });
 
   } catch (err) {
     console.error(err);
-    res.status(httpStatus.INTERNAL_SERVER).json(INTERNAL_ERROR_MESSAGE);
+    res.status(httpStatus.INTERNAL_SERVER).json({ message: errorMessages.INTERNAL_ERROR });
   }
 };
 
--- services/talkersService.js
+++ services/talkersService.js
@@ -1,14 +1,14 @@
 const talkersModel = require('../models/talkersModel');
+const { errorMessages } = require('../helpers');
 
-const EMAIL_REQUIRED = { message: 'O campo "email" é obrigatório' };
-const INVALID_EMAIL = { message: 'O "email" deve ter o formato "email@email.com"' };
-const PASSWORD_REQUIRED = { message: 'O campo "password" é obrigatório' };
-const INVALID_PASSWORD = { message: 'O "password" deve ter pelo menos 6 caracteres' };
+const mailFormat = /(.+)@(.+){2,}\.(.+){2,}/;
 
 const getAllTalkers = async (filePath) => {
   const talkersList = await talkersModel.getAllTalkers(filePath);
 
-  !talkersList ? [] : talkersList;
+  if (!talkersList) return [];
+
+  return talkersList;
 };
 
 const validateLogin = (email, password) => {

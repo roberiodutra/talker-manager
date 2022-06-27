985851de779142cd5588c407c6547198c3508bd2
roberio <roberio-almeida@hotmail.com>
2022-06-26 22:33:04 -0300

    fix: stage files

---
+++ helpers/errorMessages.js
@@ -0,0 +1,11 @@
+const errorMessages = {
+  INTERNAL_ERROR: 'Error when trying to perform operation',
+  NOT_FOUND: '404 Nothing Found',
+  TALKER_NOT_FOUND: 'Pessoa palestrante não encontrada',
+  EMAIL_REQUIRED: 'O campo "email" é obrigatório',
+  INVALID_EMAIL: 'O "email" deve ter o formato "email@email.com"',
+  PASSWORD_REQUIRED: 'O campo "password" é obrigatório',
+  INVALID_PASSWORD: 'O "password" deve ter pelo menos 6 caracteres'
+};
+
+module.exports = errorMessages;

f9e27ece3b6fb814b610ae8400ff5f0a3cf08084
roberio <roberio-almeida@hotmail.com>
2022-06-26 22:32:38 -0300

    feat: create case for validateLogin service

--- services/talkersService.js
+++ services/talkersService.js
@@ -12,7 +12,18 @@ const getAllTalkers = async (filePath) => {
 };
 
 const validateLogin = (email, password) => {
-
+  switch (true) {
+  case !email:
+    return errorMessages.EMAIL_REQUIRED;
+  case !email.match(mailFormat):
+    return errorMessages.INVALID_EMAIL;
+  case !password:
+    return errorMessages.PASSWORD_REQUIRED;
+  case password.length < 6:
+    return errorMessages.INVALID_PASSWORD;
+  default:
+    return false;
+  }
 }
 
 module.exports = {

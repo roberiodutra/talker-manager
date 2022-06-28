bb1260727e99b9b7cf89f35e380dcd31e23dc1de
roberio <roberio-almeida@hotmail.com>
2022-06-28 16:15:02 -0300

    feat: add 'remove'func controller in router

--- routers/talkersRouter.js
+++ routers/talkersRouter.js
@@ -10,6 +10,6 @@ router.get('/talker/:id', talkersController.getTalkerById);
 router.post('/login', talkersController.userLogin);
 router.post('/talker', tokenAuth, bodyValidation, crudController.add);
 router.put('/talker/:id', tokenAuth, bodyValidation, crudController.edit);
-router.delete('/talker/:id', tokenAuth);
+router.delete('/talker/:id', tokenAuth, crudController.remove);
 
 module.exports = router;

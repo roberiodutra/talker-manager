b52915772f9a36fd2b9b0896890aa90a0119637f
roberio <roberio-almeida@hotmail.com>
2022-06-28 16:14:02 -0300

    feat: new controller func, to remove talkers

--- controllers/crudController.js
+++ controllers/crudController.js
@@ -6,7 +6,6 @@ const TALKERS_FILE = './talker.json';
 const add = async (req, res) => {
   try {
     const newTalker = await crudService.add(TALKERS_FILE, req);
-
     return res.status(httpStatus.CREATED).json(newTalker);
   } catch (err) {
     console.error(err);
@@ -24,4 +23,14 @@ const edit = async (req, res) => {
   }
 };
 
-module.exports = { add, edit };
+const remove = async (req, res) => {
+  try {
+    await crudService.remove(TALKERS_FILE, req);
+    return res.status(httpStatus.NO_CONTENT).end();
+  } catch (err) {
+    console.error(err);
+    res.status(httpStatus.INTERNAL_SERVER).json(errorMessages.INTERNAL_ERROR);
+  }
+};
+
+module.exports = { add, edit, remove };
